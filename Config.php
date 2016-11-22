b5a7f1ee9db477764cbb5bfb207c1361f535e3c2
luis15 <coelho.luisantonio@hotmail.com>
2016-11-22 16:09:02 -0200

    local options

--- Config.php
+++ Config.php
@@ -1,8 +1,14 @@
 <?php
+/*
 	define('DB_USERNAME','u979108798_usu');
 	define('DB_PASSWORD','server123');
 	define('DB_NAME','u979108798_bd');
 	define('DB_HOST','mysql.hostinger.com.br');
+*/
 
-	define('FIREBASE_API_KEY', 'AIzaSyB3SX--a9w7hmEmkC8ohY1MZ-SudEUTbTk
-');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','server');
+define('DB_HOST','localhost');
+
+	define('FIREBASE_API_KEY', 'AIzaSyB3SX--a9w7hmEmkC8ohY1MZ-SudEUTbTk');
